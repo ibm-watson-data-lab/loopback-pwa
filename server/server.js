8b5d4def35bc191c826d3634a14f99fbdb16845f
Bradley Holt <bradley.holt@us.ibm.com>
2018-03-26 17:16:47 -0400

    Use the spdy module and enable HTTP/2 and HTTPS

--- server/server.js
+++ server/server.js
@@ -3,14 +3,21 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
+var https = require('spdy');
+var sslConfig = require('./ssl-config');
+
 var app = module.exports = loopback();
 
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+  var httpsOptions = {
+    key: sslConfig.privateKey,
+    cert: sslConfig.certificate,
+  };
+  // start the HTTPS web server
+  return https.createServer(httpsOptions, app).listen(app.get('port'), function() {
+    var baseUrl = 'https://' + app.get('host') + ':' + app.get('port');
+    app.emit('started HTTPS web server', baseUrl);
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
