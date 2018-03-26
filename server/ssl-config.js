0e8a7d213fab3b89b2d0ee435d30e2acb417268d
Bradley Holt <bradley.holt@us.ibm.com>
2018-03-26 17:14:31 -0400

    Create an SSL config file

---
+++ server/ssl-config.js
@@ -0,0 +1,5 @@
+var path = require('path');
+var fs = require('fs');
+
+exports.privateKey = fs.readFileSync(path.join(__dirname, './private/localhost.key.pem')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, './private/localhost.cert.pem')).toString();
